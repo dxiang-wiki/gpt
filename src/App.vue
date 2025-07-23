635cf8f98ea3620157cf19c11c501935eea1d919
root <root@inss-655893aea3356378b062226c-iprj6880c054cbf5520f73901dd9.inss-655893aea3356378b062226c-iprj6880c054cbf5520f73901dd9.inscode.svc.cluster.local>
2025-07-23 19:08:29 +0800

    Auto Commit

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,10 @@
   <div class="container ivu-p">
     <div class="dialog">
       <template v-for="(item, index) in dialogs" :key="index">
-        <div
-          class="dialog-item"
-          :class="{
+        <div class="dialog-item" :class="{
             'dialog-item-me': item.role === 'me',
             'dialog-item-ai': item.role === 'ai',
-          }"
-        >
+          }">
           <div class="dialog-item-main">{{ item.text }}</div>
         </div>
       </template>
@@ -22,24 +19,22 @@
       />
       <Row class="ivu-mt">
         <Col>
-          <Button
+        <Button
             type="primary"
             size="large"
             icon="md-send"
             :loading="loading"
             @click="handleSend"
-            >发送</Button
-          >
+            >发送</Button>
         </Col>
         <Col>
-          <Button
+        <Button
             size="large"
             class="ivu-ml"
             icon="md-add"
             :disabled="loading"
             @click="handleNewChat"
-            >新对话</Button
-          >
+            >新对话</Button>
         </Col>
       </Row>
       <Typography class="ivu-text-center ivu-m">
@@ -50,7 +45,7 @@
   </div>
 </template>
 <script>
-import { fetchEventSource } from "@microsoft/fetch-event-source";
+  import { fetchEventSource } from "@microsoft/fetch-event-source";
 import { apiKey, apiUrl } from "./api";
 
 export default {
@@ -135,46 +130,46 @@ export default {
 };
 </script>
 <style>
-.container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-}
+  .container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
 
-.dialog {
-  flex: 1;
-  overflow: auto;
-}
+  .dialog {
+    flex: 1;
+    overflow: auto;
+  }
 
-.dialog-item {
-  display: flex;
-}
+  .dialog-item {
+    display: flex;
+  }
 
-.dialog-item-main {
-  max-width: 80%;
-  padding: 8px;
-  word-wrap: break-word;
-  margin-top: 16px;
-  border-radius: 4px;
-}
+  .dialog-item-main {
+    max-width: 80%;
+    padding: 8px;
+    word-wrap: break-word;
+    margin-top: 16px;
+    border-radius: 4px;
+  }
 
-.dialog-item-me {
-  justify-content: flex-end;
-}
+  .dialog-item-me {
+    justify-content: flex-end;
+  }
 
-.dialog-item-me .dialog-item-main {
-  background-color: antiquewhite;
-}
+  .dialog-item-me .dialog-item-main {
+    background-color: antiquewhite;
+  }
 
-.dialog-item-ai .dialog-item-main {
-  background-color: #eee;
-}
+  .dialog-item-ai .dialog-item-main {
+    background-color: #eee;
+  }
 
-.logo {
-  width: 16px;
-  height: 16px;
-  vertical-align: middle;
-  position: relative;
-  top: -1px;
-}
+  .logo {
+    width: 16px;
+    height: 16px;
+    vertical-align: middle;
+    position: relative;
+    top: -1px;
+  }
 </style>
